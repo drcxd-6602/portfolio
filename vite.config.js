84c671fa30df77121d13d04f4c1ff5e908b12786
Darshan Chaudhari <darshan.chaudhari.6602@gmail.com>
2026-01-09 00:39:30 +0530

    chore: modify base path

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: "/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ['**/*.md'],
+  assetsInclude: ["**/*.md"],
 });
